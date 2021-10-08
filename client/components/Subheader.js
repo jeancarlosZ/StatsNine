f0430096b7b83e9401ee5b5b81b77b008d8ea791
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-10-08 12:55:04 -0400

    Fit navbar and overview in place

--- client/components/Subheader.js
+++ client/components/Subheader.js
@@ -4,9 +4,9 @@ export default function Subheader() {
   return (
     <div>
       <nav className="sub-header">
-        <button>Overview</button>
-        <button>Key Metrics</button>
-        <button>Financials</button>
+        <button className="buttons sub-button">Overview</button>
+        <button className="buttons sub-button">Key Metrics</button>
+        <button className="buttons sub-button">Financials</button>
       </nav>
     </div>
   );
