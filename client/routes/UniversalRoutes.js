e6757998b0b7c3c0b0d85c665506848ed347d918
TehBunk <tehbunk@gmail.com>
2021-10-08 13:35:37 -0500

    starting on key metrics

--- client/routes/UniversalRoutes.js
+++ client/routes/UniversalRoutes.js
@@ -1,16 +1,15 @@
+import React, { Component } from 'react'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
-import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
-
-import Homepage from '../components/generalpages/homepage/Homepage';
-import Overviewpage from '../components/overviewcontainer/Overviewpage';
-import Overview from '../components/overviewcontainer/overview/Overview';
-import Financials from '../components/overviewcontainer/fin/Financialspage';
+import Homepage from '../components/generalpages/homepage/Homepage'
+import Overviewpage from '../components/overviewcontainer/Overviewpage'
+import Overview from '../components/overviewcontainer/overview/Overview'
+import Financials from '../components/overviewcontainer/fin/Financialspage'
 import ExamplePage from '../api/ExamplePage'
 import TestPage from '../api/TestPage'
-import AboutUs from "../components/generalpages/aboutus/AboutUs";
-import Table from "../components/screener/Table";
-
+import AboutUs from '../components/generalpages/aboutus/AboutUs'
+import Table from '../components/screener/Table'
+import KeyMetrics from '../components/overviewcontainer/keymetrics/KeyMetrics'
 
 //* ^ Import components ^
 
@@ -18,6 +17,7 @@ class UniversalRoutes extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/overviewpage/keymetrics" component={KeyMetrics} />
         {/* <Route path="/about" component={About} /> */}
 
         <Route path="/test" component={TestPage} />
@@ -25,6 +25,7 @@ class UniversalRoutes extends Component {
         <Route path="/screener" component={Table} />
         <Route path="/aboutus" component={AboutUs} />
         <Route path="/home" component={Homepage} />
+        <Route exact path="/" component={Homepage} />
         {/* <Route exact path="/" component={Homepage} /> */}
         <Route exact path="/overviewpage" component={Overviewpage} />
         <Route exact path="/overviewpage/financials" component={Financials} />
