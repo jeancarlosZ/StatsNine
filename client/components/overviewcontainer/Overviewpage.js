08e33415773aa342a48670cf5a762757f06624f9
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-10-08 14:08:28 -0400

    Fixed some CSS bugs and now have basic navigation from overview page

--- client/components/overviewcontainer/Overviewpage.js
+++ client/components/overviewcontainer/Overviewpage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Subheader from '../Subheader';
-import Overview from '../overviewcontainer/overview/Overview';
 
 export default function Overviewpage() {
   return (
     <div>
       <Subheader />
-      <Overview />
     </div>
   );
 }
