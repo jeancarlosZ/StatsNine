474714da69c58322ec54a0d0be216aa11d779a76
AI21212019 <quimusa7@gmail.com>
2021-10-08 11:38:30 -0400

    homepage:progress

--- client/components/generalpages/homepage/Homepage.js
+++ client/components/generalpages/homepage/Homepage.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
 export default function Homepage() {
-  return <div></div>;
+  return (
+    <div>
+      {/* <div className="jumbotron text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
 }
