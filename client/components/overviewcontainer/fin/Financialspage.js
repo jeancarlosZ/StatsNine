4ac4f3ac306d18515cb05b97dfa08221538c5bdc
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-10-08 09:33:26 -0400

    Working on financials/income page

--- client/components/overviewcontainer/fin/Financialspage.js
+++ client/components/overviewcontainer/fin/Financialspage.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import UniversalChart from '../../UniversalChart';
 import { Price } from '../PriceChart';
+import Income from './Income';
 
 export default function Financialspage() {
   return (
-    <div className="fin-container flex-col justify-around">
-      <FinancialsNavBar />
-      <div className="income-container flex-col">
-        <span className="flex-row justify-around">
-          <CompanyInfo />
-          <IncomeChart />
-        </span>
+    <div className="main flex-col justify-center">
+      <div className="card flex-col align-self justify-around">
+        <FinancialsNavBar />
+        <Income />
       </div>
-      <Table />
     </div>
   );
 }
@@ -20,52 +17,10 @@ export default function Financialspage() {
 function FinancialsNavBar() {
   return (
     <nav className="fin-nav">
-      <button>Income Statement</button>
-      <button>Balance Sheet</button>
-      <button>Cash Flow</button>
-      <button>Dividends</button>
+      <button className="buttons nav-button">Income Statement</button>
+      <button className="buttons nav-button inactive">Balance Sheet</button>
+      <button className="buttons nav-button inactive">Cash Flow</button>
+      <button className="buttons nav-button inactive">Dividends</button>
     </nav>
   );
-}
-function CompanyInfo() {
-  return (
-    <div className="company-container align-self flex-col">
-      <div className="company-info">
-        <span className="company-name">MICROSOFT CORP.</span>
-        <div className="ticker-container flex-row justify-between">
-          <span className="ticker">MSFT</span>
-          <span className="market">NASDAQ</span>
-        </div>
-        <span className="income-price">
-          <Price />
-        </span>
-      </div>
-    </div>
-  );
-}
-
-function IncomeChart() {
-  const dataset = [];
-
-  dataset.push({
-    name: 'Income',
-    type: 'bar',
-    labels: ['1st', '2nd', '3rd', '4th', '5th'],
-    values: [38, 27, 18, 10, 7],
-    hoverinfo: 'label+percent+name',
-    domain: { row: 1, column: 0 },
-  });
-
-  return (
-    <UniversalChart
-      className="income-statement-chart"
-      title="Net Income"
-      dataset={dataset}
-      showlegend={false}
-    />
-  );
-}
-
-function Table() {
-  return <div className="table">Table</div>;
 }
--- client/components/overviewcontainer/fin/Income.js
+++ client/components/overviewcontainer/fin/Income.js
@@ -1,5 +1,68 @@
 import React from 'react';
+import UniversalChart from '../../UniversalChart';
+import { Price } from '../PriceChart';
 
 export default function Income() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="income-container flex-row justify-around">
+        <CompanyInfo />
+        <IncomeChart />
+      </div>
+      <Buttons />
+      <Table />
+    </div>
+  );
+}
+
+function CompanyInfo() {
+  return (
+    <div className="company-container align-self flex-col">
+      <div className="company-info">
+        <span className="company-name">MICROSOFT CORP.</span>
+        <div className="ticker-container flex-row justify-between">
+          <span className="ticker">MSFT</span>
+          <span className="bold">NASDAQ</span>
+        </div>
+        <span className="bold ">
+          <Price />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function IncomeChart() {
+  const dataset = [];
+
+  dataset.push({
+    name: 'Income',
+    type: 'bar',
+    labels: ['1st', '2nd', '3rd', '4th', '5th'],
+    values: [38, 27, 18, 10, 7],
+    hoverinfo: 'label+percent+name',
+    domain: { row: 1, column: 0 },
+  });
+
+  return (
+    <UniversalChart
+      className="income-statement-chart"
+      title="Net Income"
+      dataset={dataset}
+      showlegend={false}
+    />
+  );
+}
+
+function Table() {
+  return <div className="table">Table</div>;
+}
+
+function Buttons() {
+  return (
+    <div className="fin-button-container flex-row justify-between align-self">
+      <button className="buttons">Annual</button>
+      <button className="buttons">Quarterly</button>
+    </div>
+  );
 }
