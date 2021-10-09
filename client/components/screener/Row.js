4fc9eac275d48dfbcabece013668221aabd823d4
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-10-08 23:54:06 -0400

    feat(comp/screener): Screener can render data for 500 stocks

--- client/components/screener/Row.js
+++ client/components/screener/Row.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import { roundNumberDec, formatNumber } from "../../utils";
 
-export default function Row() {
-  return <div></div>;
+export default function Row(props) {
+  let { companyName, lastAnnualDividend, marketCap, price, sector, symbol, volume } = props.stock;
+  price = roundNumberDec(price).toFixed(2);
+  lastAnnualDividend = roundNumberDec(lastAnnualDividend).toFixed(2);
+  volume = formatNumber(volume);
+  marketCap = formatNumber(marketCap);
+
+  return (
+    <tr>
+      <td>
+        <div>{symbol}</div>
+        <div>{companyName}</div>
+      </td>
+      <td>{price}</td>
+      <td>{lastAnnualDividend}</td>
+      <td>{volume}</td>
+      <td>{marketCap}</td>
+      <td>{sector}</td>
+    </tr>
+  );
 }
--- client/components/screener/Table.js
+++ client/components/screener/Table.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchScreenerStocks } from "../../api/api";
+import Row from "./Row";
 
 export default function Table() {
-  return <div></div>;
+  const [stocksList, setStocksList] = useState([]);
+
+  useEffect(() => {
+    async function getStocksList() {
+      setStocksList(await fetchScreenerStocks());
+    }
+
+    getStocksList();
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol and Name</th>
+            <th>Last Price</th>
+            <th>Annual Dividend</th>
+            <th>Volume</th>
+            <th>Market Cap</th>
+            <th>Sector</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocksList.length ? (
+            stocksList.map(stock => <Row key={stock.symbol} stock={stock} />)
+          ) : (
+            <tr>
+              <td>Maximum API calls have been reached! Please try again later.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
