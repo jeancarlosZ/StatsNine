657ab31554e92284d401b9f6c627e89234ebfadc
AI21212019 <quimusa7@gmail.com>
2021-10-10 15:29:18 -0400

    feat(homepage): renames folders to home and common for front page components

--- client/routes/UniversalRoutes.js
+++ client/routes/UniversalRoutes.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
-import Homepage from '../components/generalpages/homepage/Homepage';
+import Homepage from '../components/home/Homepage';
 import Overviewpage from '../components/overviewcontainer/Overviewpage';
 import ExamplePage from '../api/ExamplePage';
 import TestPage from '../api/TestPage';
-import AboutUs from '../components/generalpages/aboutus/AboutUs';
+import AboutUs from '../components/home/aboutus';
 import Table from '../components/screener/Table';
 //import KeyMetrics from '../components/overviewcontainer/keymetrics/KeyMetrics';
 import Financialspage from '../components/overviewcontainer/fin/Financialspage';
@@ -13,7 +13,7 @@ import Income from '../components/overviewcontainer/fin/Income';
 import Balance from '../components/overviewcontainer/fin/Balance';
 import Cash from '../components/overviewcontainer/fin/Cash';
 import Dividends from '../components/overviewcontainer/fin/Dividends';
-import NotFound from '../components/generalpages/homepage/NotFound';
+import NotFound from '../components/home/NotFound';
 
 //* ^ Import components ^
 
