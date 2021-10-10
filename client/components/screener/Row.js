2e8af7e50054b0ea748cca0cd16d2ad73b3a9a3e
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-10-09 22:15:36 -0400

    feat(comp & CSS): screener looks better and can send user to overview page

--- client/components/screener/Row.js
+++ client/components/screener/Row.js
@@ -1,24 +1,35 @@
-import React from "react";
-import { roundNumberDec, formatNumber } from "../../utils";
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { roundNumberDec, formatNumber } from '../../utils'
+import { setTickerSymbol } from '../../store/local/localActions'
 
 export default function Row(props) {
-  let { companyName, lastAnnualDividend, marketCap, price, sector, symbol, volume } = props.stock;
-  price = roundNumberDec(price).toFixed(2);
-  lastAnnualDividend = roundNumberDec(lastAnnualDividend).toFixed(2);
-  volume = formatNumber(volume);
-  marketCap = formatNumber(marketCap);
+  let { companyName, lastAnnualDividend, marketCap, price, sector, symbol, volume } = props.stock
+  price = roundNumberDec(price).toFixed(2)
+  lastAnnualDividend = roundNumberDec(lastAnnualDividend).toFixed(2)
+  volume = formatNumber(volume)
+  marketCap = formatNumber(marketCap)
+  const rowColor = props.index % 2 ? 'screen-color' : 'screen-color screen-row'
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  async function handleClick(ticker) {
+    await dispatch(setTickerSymbol(ticker))
+    await history.push('/overviewpage')
+  }
 
   return (
-    <tr>
-      <td>
-        <div>{symbol}</div>
+    <tr className={rowColor} onClick={() => handleClick(symbol)}>
+      <td className='screen-border screen-name'>
+        <div className='screen-blue'>{symbol}</div>
         <div>{companyName}</div>
       </td>
-      <td>{price}</td>
-      <td>{lastAnnualDividend}</td>
-      <td>{volume}</td>
-      <td>{marketCap}</td>
-      <td>{sector}</td>
+      <td className='screen-border screen-num'>{price} USD</td>
+      <td className='screen-border screen-num screen-green'>{lastAnnualDividend} USD</td>
+      <td className='screen-border screen-num'>{volume}</td>
+      <td className='screen-border screen-num'>{marketCap} USD</td>
+      <td className='screen-border'>{sector}</td>
     </tr>
-  );
+  )
 }
