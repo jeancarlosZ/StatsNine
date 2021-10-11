4a0a34eb76b7793839ec865b96505d8a6196702f
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-10-10 23:18:33 -0400

    last commit for the night

--- client/components/overviewcontainer/fin/CompanyInfo.js
+++ client/components/overviewcontainer/fin/CompanyInfo.js
@@ -5,9 +5,9 @@ export default function CompanyInfo(props) {
     <div className="company-container pos-rel flex-col">
       <div className="company-info pos-rel">
         <span className="company-name">{props.companyName}</span>
-        <div className="ticker-container flex-row justify-between">
-          <span className="ticker bold">{props.symbol}</span>
+        <div className="ticker-container flex-col justify-between">
           <span className="bold">{props.ticker}</span>
+          <span className="ticker bold">{props.symbol}</span>
         </div>
       </div>
     </div>
