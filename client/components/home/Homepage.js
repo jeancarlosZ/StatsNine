89d8afd779ca9e3b85bcb6cf1a9a3b3c4ab4ae58
TehBunk <tehbunk@gmail.com>
2021-10-13 16:59:37 -0500

    fixed more stuff

--- client/components/home/Homepage.js
+++ client/components/home/Homepage.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
-import Newsfeed from './Newsfeed';
-import SnP500 from './SnP500';
-import Calendar from './Calendar';
-import Alert from '../home/common/Alert';
-import Pic from '../home/common/pic.png';
+import Newsfeed from './Newsfeed'
+import SnP500 from './SnP500'
+import Calendar from './Calendar'
+import Alert from '../home/common/Alert'
+// import Pic from '../home/common/pic.png';
 
 export default function Homepage() {
   return (
     <div>
       <div>
         <Card className="bg-dark text-white">
-          <Card.Img src={Pic} alt="NYC" style={{ filter: 'brightness(30%)' }} />
+          <Card.Img src="./images/dashboard.jpg" alt="NYC" style={{ filter: 'brightness(30%)' }} />
           <Card.ImgOverlay>
             <Container fluid>
               <Row>
@@ -36,5 +36,5 @@ export default function Homepage() {
         </Card>
       </div>
     </div>
-  );
+  )
 }
