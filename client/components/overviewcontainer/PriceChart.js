a18d3d6eae5355414355047913e2748fa4682ab2
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-10-12 20:35:18 -0400

    feat(comp & CSS): made the candlestick chart render with minimum api calls by working with the redux store, made the CSS looks a bit better, and added a selection of time ranges for the chart

--- client/components/overviewcontainer/PriceChart.js
+++ client/components/overviewcontainer/PriceChart.js
@@ -1,64 +1,117 @@
 import React, { useEffect, useState } from 'react'
-import { DAILY, fetchChartPrice, YEAR } from '../../api/api'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import { DAILY, fetchChartPrice, WEEK, MONTH, THREE_MONTH, SIX_MONTH, YEAR } from '../../api/api'
+import { getLocalData } from '../../store/local/localActions'
 import UniversalChart from '../UniversalChart'
 
 export default function PriceChart(props) {
   const symbol = props.symbol
-  const [stockPrices, setStockPrices] = useState([])
+  const [range, setRange] = useState(YEAR)
+  const [series, setSeries] = useState(DAILY)
+  const [data, setData] = useState({})
+  const [update, setUpdate] = useState(true)
+  let price = 0
 
   useEffect(() => {
     // This function sets the stock information from the API call to local storage.
-    async function getStockPrices() {
+    async function getData() {
       try {
-        setStockPrices(await fetchChartPrice(symbol, DAILY, YEAR, false))
+        if (update) {
+          setData({
+            ...data,
+            [range]: await getLocalData(
+              ['open', 'close', 'low', 'high'],
+              fetchChartPrice,
+              [series, range, false],
+              [
+                `price${series}${range}open`,
+                `price${series}${range}close`,
+                `price${series}${range}low`,
+                `price${series}${range}high`,
+              ],
+            ),
+          })
+          setUpdate(false)
+        }
       } catch (err) {
         console.log(err)
       }
     }
 
-    getStockPrices()
-  }, [symbol])
+    getData()
+  }, [symbol, range, series])
+
+  function updateSeries(series, newSeries) {
+    if (series !== newSeries) {
+      setSeries(newSeries)
+      setUpdate(true)
+    }
+  }
+
+  function updateRange(range, newRange) {
+    if (range !== newRange) {
+      setRange(newRange)
+      if (!data[newRange]) {
+        setUpdate(true)
+      }
+    }
+  }
+
+  if (!update) {
+    price = data[range].close.values[0]
+  }
 
   return (
     <div className='price-chart flex-col align-self'>
-      <span className='price-container flex-row pos-rel'>
-        <Price stockPrices={stockPrices} />
-        <USD />
+      <span className='price-container flex-row'>
+        ${price.toFixed(2)} USD
+        {getSelectors(series, range, updateSeries, updateRange)}
       </span>
-      <OverviewChart stockPrices={stockPrices} symbol={symbol} />
+      <CandleStickChart update={update} symbol={symbol} data={data[range]} />
     </div>
   )
 }
 
 // This function uses information from props to render a candlestick chart.
-function OverviewChart(props) {
+export function CandleStickChart(props) {
   const dataset = []
-  const keys = props.stockPrices.keys
-  const values = props.stockPrices.values
-  const symbol = props.symbol
+  let symbol = ''
+  let keys = []
+  let close = []
+  let high = []
+  let low = []
+  let open = []
 
-  if (values.length) {
-    dataset.push({
-      customSet: {
-        name: `${symbol} Stock Chart`,
-        type: 'candlestick',
-        x: keys,
-        close: values.map(e => e.close),
-        decreasing: { line: { color: 'red' } },
-        high: values.map(e => e.high),
-        increasing: { line: { color: 'green' } },
-        line: { color: 'yellow' },
-        low: values.map(e => e.low),
-        open: values.map(e => e.open),
-        xaxis: 'x',
-        yaxis: 'y',
-      },
-    })
+  if (!props.update) {
+    symbol = props.symbol
+    keys = props.data.close.keys
+    close = props.data.close.values
+    high = props.data.high.values
+    low = props.data.low.values
+    open = props.data.open.values
   }
+
+  dataset.push({
+    customSet: {
+      name: `${symbol} Stock Chart`,
+      type: 'candlestick',
+      x: keys,
+      close,
+      decreasing: { line: { color: 'red' } },
+      high,
+      increasing: { line: { color: 'green' } },
+      line: { color: 'yellow' },
+      low,
+      open,
+      xaxis: 'x',
+      yaxis: 'y',
+    },
+  })
 
   return (
     <UniversalChart
-      className='stock-price-chart'
+      className='candlestick-chart'
       title={`${symbol} Stock Chart`}
       dataset={dataset}
       showlegend={false}
@@ -66,18 +119,16 @@ function OverviewChart(props) {
   )
 }
 
-// This function displays the price of the stock from information from props.
-function Price(props) {
-  let price = 0
-
-  if (props.stockPrices.values[0]) {
-    price = props.stockPrices.values[0].close.toFixed(2)
-  }
-
-  return <div className='bold'>${price}</div>
-}
-
-// This function generates a fancy looking text with the following content: 'USD'.
-function USD() {
-  return <div className='usd pos-rel'>USD</div>
+function getSelectors(series, range, updateSeries, updateRange) {
+  return (
+    <div className='selector-dropdown-right'>
+      <DropdownButton className='dropdown-selector' title={range} size='sm' variant='secondary'>
+        <Dropdown.Item onClick={() => updateRange(range, YEAR)}>1 Year</Dropdown.Item>
+        <Dropdown.Item onClick={() => updateRange(range, SIX_MONTH)}>6 Months</Dropdown.Item>
+        <Dropdown.Item onClick={() => updateRange(range, THREE_MONTH)}>3 Months</Dropdown.Item>
+        <Dropdown.Item onClick={() => updateRange(range, MONTH)}>1 Month</Dropdown.Item>
+        <Dropdown.Item onClick={() => updateRange(range, WEEK)}>1 Week</Dropdown.Item>
+      </DropdownButton>
+    </div>
+  )
 }
