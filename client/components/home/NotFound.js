44a8dda1e55421e46f715aff0b54926ca388319f
TehBunk <tehbunk@gmail.com>
2021-10-17 16:24:10 -0500

    not-found page

--- client/components/home/NotFound.js
+++ client/components/home/NotFound.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from 'react'
+import { useHistory } from 'react-router'
 
-const NotFound = () => {
-  return <h1>Not Found</h1>;
-};
-
-export default NotFound;
+export default function NotFound() {
+  return (
+    <div className="loading-page">
+      <div className="lcontainer">
+        <img src="https://c.tenor.com/JrGZjfUdpMAAAAAC/bugs-bunny-albuquerque.gif" alt="" />
+        <label className="zoomable">Oops. It look's like you've taken a wrong turn!</label>
+      </div>
+    </div>
+  )
+}
